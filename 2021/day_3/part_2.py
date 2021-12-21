d4b59137ee4f193a33f083fbf694e33bf9c9303f
Ishai Masada <imasada@vtcap.org>
2021-12-21 09:28:49 -0500

    fixed em up
    put the code that cleared the digit each time at the bottom of the iteration

--- 2021/day_3/part_2.py
+++ 2021/day_3/part_2.py
@@ -4,22 +4,22 @@
 
 import re
 
-# with open('num_list.txt', 'r') as f:
-#     bin_nums = f.read()
+with open('num_list.txt', 'r') as f:
+    bin_nums = f.read()
 
-bin_nums = '''
-00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010'''
+# bin_nums = '''
+# 00100
+# 11110
+# 10110
+# 10111
+# 10101
+# 01111
+# 00111
+# 11100
+# 10000
+# 11001
+# 00010
+# 01010'''
 
 bin_nums = [num for num in bin_nums.splitlines() if num]
 digits = []
@@ -28,10 +28,6 @@ nums = []
 for _ in range(0, len(bin_nums[0])):
     digits.append([])
 
-for num in bin_nums:
-    for idx, digit in enumerate(digits):
-        digit.append(num[idx])
-
 def o2(digits, bin_nums):
     for idx, digit in enumerate(digits):
         if len(bin_nums) == 1:
@@ -39,57 +35,67 @@ def o2(digits, bin_nums):
 
         # If the number of 1s is greater than the number of 0s, keep the numbers with a 1 in
         # the current position
-        elif digit.count('1') > digit.count('0'):
+        if digit.count('1') > digit.count('0'):
             bin_nums = [num for num in bin_nums if num[idx] == '1']
-            digit.remove('0')
 
         # If the number of 1s is less than the number of 0s, keep the numbers with a 0 in
         # the current position
         elif digit.count('1') < digit.count('0'):
             bin_nums = [num for num in bin_nums if num[idx] == '0']
-            digit.remove('1')
 
         # If the number of 1s is equal to the number of 0s, keep the numbers with a 1 in
         # the current position
         elif digit.count('1') == digit.count('0'):
             bin_nums = [num for num in bin_nums if num[idx] == '1']
-            digit.remove('0')
 
+        # Remove all numbers from each digit in digits
+        for digit in digits:
+            digit.clear()
+
+        # Append each digit from each position of each number in bin_nums
+        for num in bin_nums:
+            for idx, digit in enumerate(digits):
+                digit.append(num[idx])
 
     o2 = int(bin_nums[0], 2)
-    print(o2)
     return o2
 
 
-def co2():
+def co2(digits, bin_nums):
     for idx, digit in enumerate(digits):
-
         if len(bin_nums) == 1:
             break
 
-        # If the number of 1s is greater than the number of 0s, keep the numbers with a 0 in
+        # If the number of 1s is greater than the number of 0s, keep the numbers with a 1 in
         # the current position
-        elif digit.count('1') < digit.count('0'):
+        if digit.count('1') < digit.count('0'):
             bin_nums = [num for num in bin_nums if num[idx] == '1']
 
-        # If the number of 1s is less than the number of 0s, keep the numbers with a 1 in
-        # the current position
-        elif digit.count('1') == digit.count('0'):
-            bin_nums = [num for num in bin_nums if num[idx] == '0']
-
-        # If the number of 1s is equal to the number of 0s, keep the numbers with a 1 in
+        # If the number of 1s is less than the number of 0s, keep the numbers with a 0 in
         # the current position
         elif digit.count('1') > digit.count('0'):
             bin_nums = [num for num in bin_nums if num[idx] == '0']
 
-    co2 = int(bin_nums[0], 2)
-    print(co2)
+        # If the number of 1s is equal to the number of 0s, keep the numbers with a 1 in
+        # the current position
+        elif digit.count('1') == digit.count('0'):
+            bin_nums = [num for num in bin_nums if num[idx] == '0']
 
+        # Remove all numbers from each digit in digits
+        for digit in digits:
+            digit.clear()
+
+        # Append each digit from each position of each number in bin_nums
+        for num in bin_nums:
+            for idx, digit in enumerate(digits):
+                digit.append(num[idx])
+
+    co2 = int(bin_nums[0], 2)
     return co2
 
-o2(digits, bin_nums)
-# co2(digits, bin_nums)
+o2 = o2(digits, bin_nums)
+co2 = co2(digits, bin_nums)
 
 
-# life_support_rating = co2*o2
-# print(life_support_rating)
+life_support_rating = co2*o2
+print(life_support_rating)
