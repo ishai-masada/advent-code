138329c192c2828ad8001ae340f7e5845884526e
Ishai Masada <imasada@vtcap.org>
2021-12-31 23:26:27 -0500

    got input from day 7.
    Wrote code to load in the input.

---
+++ 2021/day_7/part_1.py
@@ -0,0 +1,12 @@
+# Day 7
+# Crab Submarines
+# Ishai Masada
+
+x_positions = [16,1,2,0,4,2,7,1,2,14]
+x_positions.sort()
+
+# with open('input.txt', 'r') as f:
+#     x_positions = [int(num) for num in f.read() if num.isnumeric()]
+
+x_positions.sort()
+
