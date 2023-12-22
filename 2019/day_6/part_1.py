b292da140d13ec54fa9c36c69301e67f739f3a3e
root <root@AEIM.localdomain>
2023-12-21 20:25:35 -0500

    reorganized the files
    - orbits.py is split into part_1.py and part_2.py
    - input.txt is now data.txt
    - Thers is now a directory for each day

--- 2019/day_6/part_1.py
+++ 2019/day_6/part_1.py
@@ -2,14 +2,19 @@
 Day 6 Part 1
 '''
 
+# Read in data
 with open('data.txt', 'r') as f:
     orbits = f.read()
 
+# Store the data with label being the body orbiting the body as the value
 orbits = {body.split(')')[1]: body.split(')')[0] for body in orbits.splitlines() if body}
 
 indirect_orbits = 0
 
+# Iterate through each body
 for body in orbits:
+    bah = 0
+    # Find every body that 
     while True:
         body = orbits.get(body)
         if body == 'COM':
